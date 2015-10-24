e75b062238e86955e89f32f5490effff62e81809
Maël Naccache <contact@maeln.com>
2015-10-24 14:32:08 +0200

    Affichage des types après création et délétion d'un types et d'un message de warning en cas de mauvais arguments.

--- RezRes/WebContent/JSP/pages/admin/gest_type.jsp
+++ RezRes/WebContent/JSP/pages/admin/gest_type.jsp
@@ -14,19 +14,37 @@
 
 	<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
 		<h1 class="page-header">${body}</h1>
+		<c:if test="${not empty warning}">
+			<div class="alert alert-warning">
+				<strong>Warning:</strong> 
+				${warning}
+			</div>
+		</c:if>
+		
 		<h2>Rajouter un type ressource :</h2>
-		<form action="/RezRes/admin/types/create" class="form-inline" role="form" method="post">
-			<label>Nom du type :</label> 
-			<input class="form-control" type="nom" name="nom">
+		<form action="/RezRes/admin/types/create" class="form-inline"
+			role="form" method="post">
+			<label>Nom du type :</label> <input class="form-control" type="nom"
+				name="nom">
 			<button type="submit" class="btn btn-default">Créer</button>
 		</form>
+
+		<c:if test="${mod_form == true}">
+			<form action="/RezRes/admin/types/modify" class="form-inline"
+				role="form" method="post">
+				<label>Nouveau nom :</label>
+				 <input class="form-control" type="nom" name="nom" value="${old_name}">
+				 <input value="${old_cle}" type="hidden" name="cle">
+				<button type="submit" class="btn btn-default">MODIFIER</button>
+			</form>
+		</c:if>
+
 		<h2>Liste des types de ressources :</h2>
-		<table class="table tact">
+		<table class="table table-bordered table-hover">
 			<thead>
 				<tr>
 					<th>ID</th>
 					<th>NOM</th>
-					<th>Actions</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -35,11 +53,15 @@
 						<td>${tres.cle}</td>
 						<td>${tres.libelle}</td>
 						<td>
-							<form action="/RezRes/admin/types/modify" class="form-inline" role="form" method="post"> 
+							<form action="/RezRes/admin/types/modify" class="form-inline"
+								role="form" method="post">
 								<input value="${tres.cle}" type="hidden" name="cle">
 								<button type="submit" class="btn btn-warning">MODIFIER</button>
 							</form>
-							<form action="/RezRes/admin/types/delete" class="form-inline" role="form" method="post"> 
+						</td>
+						<td>
+							<form action="/RezRes/admin/types/delete" class="form-inline"
+								role="form" method="post">
 								<input value="${tres.cle}" type="hidden" name="cle">
 								<button type="submit" class="btn btn-danger">SUPPRIMER</button>
 							</form>
--- RezRes/src/main/java/fil/servlets/admin/GestTypeRessources.java
+++ RezRes/src/main/java/fil/servlets/admin/GestTypeRessources.java
@@ -18,70 +18,23 @@ import fil.persistence.services.jpa.TypeRessourcePersistenceJPA;
 @WebServlet("/admin/types/*")
 public class GestTypeRessources extends HttpServlet {
 	private static final long serialVersionUID = -4391170416639320134L;
-	
+	private static final String target = "/JSP/pages/admin/gest_type.jsp";
+
 	private String getAction(HttpServletRequest request)
 	{
 		String[] uri = request.getRequestURI().split("/");
-		
+
 		if(uri.length >= 5)
 			return uri[4];
 		else
 			return uri[3];
 	}
-	
-	private void showAction(HttpServletRequest request)
+
+	private void showAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
 		List<TypeRessourceEntity> type_res = service.loadAll();
 		request.setAttribute("list_type_res", type_res);
-	}
-	
-	private void createAction(HttpServletRequest request)
-	{
-		String nom = request.getParameter("nom");
-		System.out.println(nom);
-		if(nom != null)
-		{
-			TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
-			TypeRessourceEntity new_type_res = new TypeRessourceEntity();
-			new_type_res.setLibelle(nom);
-			service.insert(new_type_res);
-		}
-	}
-	
-	private void deleteAction(HttpServletRequest request)
-	{
-		String param = request.getParameter("cle");
-		Integer cle = Integer.parseInt(param);
-		if(cle != null)
-		{
-			TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
-			service.delete(cle);
-		}
-	}
-	
-	protected void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		
-		String target = "/JSP/pages/admin/gest_type.jsp";
-		request.setAttribute("title", "RezRes - Gestion des types de ressources");
-		request.setAttribute("body", "Gestion des types de ressources");
-		request.setAttribute("menu_entry", 3);
-		
-		String action = getAction(request);
-		switch (action)
-		{
-		case "create":
-			this.createAction(request);
-			break;
-		case "delete":
-			this.deleteAction(request);
-			break;
-		case "modify":
-			break;
-		default:
-			this.showAction(request);
-		}
 		
 		RequestDispatcher rd;
 		ServletContext context = this.getServletContext();
@@ -89,10 +42,101 @@ public class GestTypeRessources extends HttpServlet {
 		rd.forward(request, response);
 	}
 	
+	/*
+	private void modifyAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String nom = request.getParameter("nom");
+		String param = request.getParameter("cle");
+		try 
+		{
+			Integer cle = Integer.parseInt(param);
+			if(nom != null && cle != null)
+			{
+				TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
+				TypeRessourceEntity new_type_res = service.load(cle);
+				new_type_res.setLibelle(nom);
+				service.insert(new_type_res);
+			}
+			else
+			{
+				request.setAttribute("mod_form", true);
+				request.setAttribute("old_name", old_name);
+			}
+		}
+		catch (NumberFormatException e)
+		{
+			request.setAttribute("warning", "Vous n'avez pas précisé de type à supprimer.");
+			this.showAction(request, response);
+		}
+		this.showAction(request, response);
+	}
+	*/
+	
+	private void createAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String nom = request.getParameter("nom");
+		if(nom != null && nom != "")
+		{
+			TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
+			TypeRessourceEntity new_type_res = new TypeRessourceEntity();
+			new_type_res.setLibelle(nom);
+			service.insert(new_type_res);
+		} 
+		else
+		{
+			request.setAttribute("warning", "Impossible de créer le type demandé : pas de nom.");
+		}
+		this.showAction(request, response);
+	}
+
+	private void deleteAction(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
+		String param = request.getParameter("cle");
+		try 
+		{
+			Integer cle = Integer.parseInt(param);
+			if(cle != null)
+			{
+				TypeRessourcePersistenceJPA service = new TypeRessourcePersistenceJPA();
+				service.delete(cle);
+			}
+			this.showAction(request, response);
+		}
+		catch (NumberFormatException e)
+		{
+			request.setAttribute("warning", "Vous n'avez pas précisé de type à supprimer.");
+			this.showAction(request, response);
+		}
+	}
+
+	protected void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		request.setAttribute("title", "RezRes - Gestion des types de ressources");
+		request.setAttribute("body", "Gestion des types de ressources");
+		request.setAttribute("menu_entry", 3);
+
+		String action = getAction(request);
+
+		switch (action)
+		{
+		case "create":
+			this.createAction(request, response);
+			break;
+		case "delete":
+			this.deleteAction(request, response);
+			break;
+		case "modify":
+			break;
+		default:
+			this.showAction(request, response);
+		}
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		handleRequest(request, response);
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		handleRequest(request, response);
 	}
